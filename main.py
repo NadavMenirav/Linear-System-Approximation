eaec301860a3eaf459bf5ba743287ba7de29d00b
Nadav Menirav <nmenirav@gmail.com>
2025-09-05 00:38:55 +0300

    (Feat) Mathmatical explanation

---
+++ main.py
@@ -0,0 +1,15 @@
+# In this program, we generate a solution x for the linear system:
+# [1, 2, -1                 [3
+#  2, 1, -2      * x    =    3
+#  -3,1, 1 ]                -6]
+
+# Using the Jacobi and Gauss-seidel Methods.
+# We Know that this methods can be viewed as x^(k+1) = M*x^k + y
+# And we know the formulas for computing M for the said methods.
+# we denote M_j as the matrix of the Jacobi method and M_gs for the Gauss-Seidel method
+import numpy as np
+
+def get_next_guess(M, x):
+    # This function will generate the next guess and will be used in both the Jacobi and
+    # Gauss-Seidel methods.
+    x_new = np.array
